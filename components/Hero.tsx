58b306308080ffa2e3a15504bca7d501b21856b0
khurrams4 <khurram.shahzad@itcurves.com>
2023-09-18 20:24:36 +0500

    image overlay...

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,8 +28,8 @@ const Hero = () => {
             fill
             className='object-contain'
             />
-            <div className='hero__image-overlay'/>
         </div>
+            <div className='hero__image-overlay'/>
       </div>
      </div>
   )
