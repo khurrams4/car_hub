ce861f63f423bd9c99e8fe7acab339bd6b600e54
khurrams4 <khurram.shahzad@itcurves.com>
2023-09-21 21:59:40 +0500

    filters...

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { SearchManufacturerProps } from '@/types';
+import { Combobox , Transition } from '@headlessui/react';
+import Image from 'next/image';
+
+
+const SearchManufacturer = ({manufacturer ,  setManufacturer }: SearchManufacturerProps) => {
+  return (
+    <div className='search-manufacturer'>
+        <Combobox>
+            <div className='realtive w-full'>
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image
+                      src="/car-logo.svg"
+                      width={20}
+                      height={20}
+                      className="ml-4"
+                      alt="Car Logo"
+                    />
+                </Combobox.Button>
+                <Combobox.Input 
+                className="search-manufacturer__input"
+                placeholder='Volkswagen'
+                displayValue={(manufacturer:string)=>manufacturer}
+                />
+
+            </div>
+
+
+        </Combobox>
+    
+    </div>
+  )
+}
+
+export default SearchManufacturer
