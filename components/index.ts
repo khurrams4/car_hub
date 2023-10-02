c455863b77b186470bc7eb0f0f8129c5db5783a7
khurrams4 <khurram.shahzad@itcurves.com>
2023-10-02 18:27:48 +0500

    almost...

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,9 @@ import NavBar from "./NavBar";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import ShowMore from './ShowMore';
+
 
 export{
     Hero,
@@ -14,4 +17,6 @@ export{
     CustomFilter,
     SearchBar,
     SearchManufacturer,
+    CarCard,
+    ShowMore,
 }
